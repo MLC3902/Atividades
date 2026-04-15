75c049a56c1057cf7dea8413bc7d1fc16aba91bd
MariaLuiza <luizacardoso0303@gmail.com>
2026-04-15 16:09:23 -0300

    Update AULA 1 (Ficha de exercícios ).c

--- AULA 1 (Ficha de exercícios ).c
+++ AULA 1 (Ficha de exercícios ).c
@@ -71,3 +71,23 @@ int main()
  	
 	return 0;
 }
+
+#include <stdio.h>  //Terceira questão letra C
+
+int main()
+{
+	float quant_fitas_total_inicio, fitas_recolocadas, fitas_estragadas, fitas_ano_todo;
+	
+	printf("Digite a quantidade total de fitas: ");
+	scanf ("%f", &quant_fitas_total_inicio);
+	
+	fitas_estragadas = 2*quant_fitas_total_inicio/100;
+	
+	fitas_recolocadas = quant_fitas_total_inicio/10;
+
+	fitas_ano_todo = quant_fitas_total_inicio+fitas_recolocadas-fitas_estragadas;
+	
+	printf("A quantidade de fitas que ter-se-a no final do ano sera: %.2f", fitas_ano_todo);
+	
+	return 0;
+}
