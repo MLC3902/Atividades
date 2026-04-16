b78a3a11ebc50dccb29582bb89d060e019a43538
MariaLuiza <luizacardoso0303@gmail.com>
2026-04-16 19:48:41 -0300

    Update AULA3 (exercicios).c

--- AULA3 (exercicios).c
+++ AULA3 (exercicios).c
@@ -25,3 +25,28 @@ int main()       //    Questão 1
 		
 	return 0;
 }
+
+
+#include <stdio.h>          //Quarta questão
+#include <locale.h>
+#include <math.h>
+
+int main()
+{
+	setlocale(LC_ALL, "Portuguese_Brazil");
+	
+	float raio, altura, vol, preco_vasilha;
+	printf("\nDigite o raio da vasilha em centímetro cúbico: \n");
+	scanf("%f", &raio);
+	
+	printf("\nDigite a altura da vasilha: ");
+	scanf("%f", &altura);
+	
+	vol = pow(raio, 2)*3.14*altura; 
+	
+	printf("\nO volume resultante em centímetros cúbicos: %.2f\n", vol);	
+	preco_vasilha = 0.2*vol;
+	
+	printf("O preco da vasilha é: %.2f", preco_vasilha);
+	return 0;
+}
